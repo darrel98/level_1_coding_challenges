4b1681175a8f1f45d104dfcadacd5a37f5c77359
unknown <pharo.md@gmail.com>
2022-05-25 14:11:09 +0200

    task 1.3 added

---
+++ task1_3.py
@@ -0,0 +1,11 @@
+def is_65(value1,value2):
+    sum = value1 + value2
+    if value1 == 65 or value2 ==65:
+        return True
+    elif sum == 65 :
+        return True
+    else:
+        return False
+
+print(is_65(65,65))
+
